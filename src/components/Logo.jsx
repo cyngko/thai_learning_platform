4c03eb2d5de101209668fb17c17a934507883e5a
Christian Boenigk <christianboenigk@gmx.de>
2020-11-25 11:48:26 +0100

    Header

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,9 @@ export default function Logo() {
   return (
     <div className='Logo'>
       <div className='flag'>
-        <div className='flagBlueStripe'></div>
         <div className='flagRedStripe'></div>
         <div className='flagBlueStripe'></div>
+        <div className='flagRedStripe'></div>
       </div>
       <p className='logoTitle'>Thaisify</p>
     </div>
